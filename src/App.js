7bdf236d308b61977df1ca284fbc4260817a5a71
RyabenkoDD <rusgamingtactics@mail.ru>
2022-10-24 16:20:26 +0700

    Forms and Routers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from "./componets/Login";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+import Register from "./componets/Register";
+import Home from "./componets/Home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+            </ul>
+          </nav>
+          </div>
+
       </header>
+
     </div>
   );
 }
+
 
 export default App;
---
+++ src/componets/Register.js
@@ -0,0 +1,79 @@
+import { FormEvent, useState } from "react";
+import styles from "./Form.module.css";
+
+export default function Login() {
+    const [login, setLogin] = useState("");
+    const [loginError, setLoginError] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [repassword, resetPassword] = useState("");
+    const [repasswordError, resetPasswordError] = useState("");
+
+    const isValid = () => {
+        let result = true;
+
+        setLoginError("");
+
+        if (login.length === 0) {
+            setLoginError("Логин не может быть пустым.");
+            result = false;
+        }
+
+        if (!/^([a-z0-9]{6,20})$/.test(login)) {
+            setLoginError("Логин должен содержать от 6 до 20 символов латинского алфавита и цифры.");
+            result = false;
+        }
+
+        setPasswordError("");
+
+        if (password.length === 0) {
+            setPasswordError("Пароль не может быть пустым.");
+            result = false;
+        }
+
+        resetPasswordError("");
+
+        if (password !== repassword) {
+            resetPasswordError("Пароль должны совпадать.");
+            result = false;
+        }
+        return result;
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (isValid()) {
+            // отправка данных на сервер
+        }
+    };
+
+    return <>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Логин:
+                    <input value={login} onChange={e => setLogin(e.target.value)}/>
+                </label>
+                {loginError && <div className={styles.error}>
+                    {loginError}
+                </div>}
+            </div>
+            <div>
+                <label>Пароль:
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                </label>
+                {passwordError && <div className={styles.error}>
+                    {passwordError}
+                </div>}
+            </div>
+            <div>
+                <label>Повтор пароля:
+                    <input type="password" value={repassword} onChange={e => resetPassword(e.target.value)}/>
+                </label>
+                {repasswordError && <div className={styles.error}>
+                    {repasswordError}
+                </div>}
+            </div>
+            <button type="submit">Зарегистрироваться</button>
+        </form>
+    </>;
+}
